f0098cd6a345ee52075fc0bcacab7a3e7eb11308
coin <918731093@qq.com>
2024-03-21 20:31:51 +0800

    delete set_exception_handler(TestHandler()) call

--- test_xhs.py
+++ test_xhs.py
@@ -7,12 +7,10 @@
 @Motto：The only one true Legendary Grandmaster.
 """
 import uiautomator2
-from ez_android_automator.client import PublishClient, TestHandler
+from ez_android_automator.client import PublishClient
 from ez_android_automator.xhs_task import XhsPublishVideoTask
 
 client = PublishClient(uiautomator2.connect_usb())
-client.set_exception_handler(TestHandler())
 client.set_task(XhsPublishVideoTask(5, '我总在', '忙忙碌碌寻宝藏', 'http://192.168.3.32:8000/media/video/17060924290060.mp4'))
 client.run_current_task()
 pass
-XhsPublishVideoTask()
