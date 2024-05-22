26f9bbced85467c556f164228fab361b87b8fdd6
coin <918731093@qq.com>
2024-05-22 23:02:58 +0800

    bilibili_task.py of complete

--- ez_android_automator/bilibili_task.py
+++ ez_android_automator/bilibili_task.py
@@ -8,7 +8,7 @@
 """
 import time
 from ez_android_automator.client import Stage, PublishClient, AndroidClient, PublishTask, DownloadMediaStage, \
-    PhoneLoginTask, WaitCallBackStage
+    PhoneLoginTask, WaitCallBackStage, StatisticTask
 
 
 class OpenAppStage(Stage):
@@ -69,6 +69,61 @@ class SetVideoOptionsStage(Stage):
         client.wait_to_click({"text": "合适的标题可以吸引更多人观看～"})
         client.device.send_keys(self.content)
         client.wait_to_click({"text": "发布"})
+
+
+class StatisticCenterStage(Stage):
+    def run(self, client: AndroidClient):
+        time.sleep(7)  # wait for ad to be finished
+        client.wait_to_click({'text': '我的'})
+        client.wait_to_click({'text': '稿件管理'})
+
+
+class GetStatisticStage(Stage):
+    def __init__(self, stage_serial: int, video_title: str, statistic_callback: callable):
+        super().__init__(stage_serial)
+        self.video_title = video_title
+        self.statistic_callback = statistic_callback
+
+    def run(self, client: AndroidClient):
+        client.wait_until_found({'text': '数据'})  # wait for list to be loaded
+        client.wait_until_found({'text': self.video_title})
+        parser = client.find_xml_by_attr({'text': self.video_title})
+        parser = parser[0]
+        parent = parser.parent.parent.parent
+        last = parent.contents[-2]
+        statistic_btn = last.find('node', {'text': '数据'})
+        client.click_xml_node(statistic_btn)
+        client.wait_until_found({'text': '播放量'})
+        time.sleep(2)
+        client.refresh_xml()
+
+        def get_statistic_by_attr_name(name):
+            label_node = client.find_xml_by_attr({'text': name})[0]
+            data_node = label_node.next.next
+            return int(data_node['text'])
+
+        statistic = {
+            'view': get_statistic_by_attr_name('播放量'),
+            'like': get_statistic_by_attr_name('点赞'),
+            'comment': get_statistic_by_attr_name('评论'),
+            'collect': get_statistic_by_attr_name('收藏'),
+            'coin': get_statistic_by_attr_name('投币'),
+            'share': get_statistic_by_attr_name('分享')
+        }
+        self.statistic_callback(statistic)
+        pass
+
+
+class BilibiliStatisticTask(StatisticTask):
+    def __init__(self, video_title):
+        super().__init__()
+        self.statistic = None
+        self.append(OpenAppStage(0))
+        self.append(StatisticCenterStage(1))
+        self.append(GetStatisticStage(2, video_title, self.statistic_callback))
+
+    def statistic_callback(self, statistic: dict):
+        self.statistic = statistic
 
 
 class BilibiliPublishVideoTask(PublishTask):
